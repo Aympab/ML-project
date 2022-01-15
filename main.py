3f2c391ee674e64e4485860563ede565ebe1b202
Aympab <aymeric.millan@mines-paristech.fr>
2022-01-15 21:07:01 +0100

    sending trees

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import numpy as np
 from zipfile import ZipFile
 from sklearn import ensemble
 from sklearn import model_selection
-from sklearn import preprocessing
+from sklearn import preprocessing, decomposition
 
 print("Loading data...")
 
@@ -17,23 +17,21 @@ X = scaler.fit_transform(X)
 X_test = scaler.transform(X_test)
 X_valid = scaler.transform(X_valid)
 
+pca = decomposition.PCA(n_components=100)
+pca.fit(X)
+X = pca.transform(X)
+X_test = pca.transform(X_test)
+X_valid = pca.transform(X_valid)
+
 bag = ensemble.RandomForestClassifier()
 
 param_grid = {
- 'max_features': ['sqrt', 'log2', 0.15],
- 'max_depth' : [10,20,50],
- 'min_weight_fraction_leaf' : [0.0,0.1,0.01,0.5],
- 'n_estimators' : [100,150,200],
- 'criterion' : ['gini', 'entropy'],
+   'max_features': [0.15, 1.0, 0.5, 0.2],
+   'max_depth' : [20],
+   'min_weight_fraction_leaf' : [0.0],
+   'n_estimators' : [100, 120, 90, 70],
+   'criterion' : ['entropy'],
 }
-
-#param_grid = {
-#    'max_features': [0.15],
-#    'max_depth' : [25],
-#    'min_weight_fraction_leaf' : [0.1],
-#    'n_estimators' : [150],
-#    'criterion' : ['entropy'],
-#}
 
 cv_bagging = model_selection.GridSearchCV(bag, param_grid=param_grid, cv=10, verbose=3, n_jobs=-1)
 
@@ -44,6 +42,7 @@ print("Fitting model...")
 cv_bagging.fit(X, y)
 
 print("score : ", cv_bagging.score(X, y))
+print(cv_bagging.best_params_)
 
 print("Predicting...")
 

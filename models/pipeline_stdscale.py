1ae55a5410e84a51084351c054a5410098053434
Aympab <aymeric.millan@mines-paristech.fr>
2022-01-22 17:35:48 +0100

    main.py try qtransformer

--- models/pipeline_stdscale.py
+++ models/pipeline_stdscale.py
@@ -4,7 +4,7 @@ from sklearn.pipeline import Pipeline
 from sklearn import model_selection
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.neighbors import KNeighborsClassifier
-from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import QuantileTransformer, StandardScaler
 from sklearn.decomposition import PCA
 from sklearn.random_projection import GaussianRandomProjection
 from sklearn.decomposition import KernelPCA
@@ -16,7 +16,7 @@ X, y, X_test, X_valid = load_data("data")
 #                                                       random_state=0)
 print("Data loaded")
 
-pipe = Pipeline([('scaler', StandardScaler()),
+pipe = Pipeline([('scaler', QuantileTransformer()),
                 ('reduction', KernelPCA()),
                 ('simple_tree', KNeighborsClassifier(algorithm='brute',
                                                         leaf_size=50,
@@ -27,9 +27,8 @@ pipe = Pipeline([('scaler', StandardScaler()),
 grid_model = model_selection.GridSearchCV(pipe,
                             #param_grid={},
                             #param_grid = {'reduction__n_components':[0.5, 0.6]},
-                            param_grid = {'reduction__n_components':[250, 400, 500, 600, 750],
-                                          'reduction__kernel':['poly', 'cosine'],
-                                          'reduction__eigen_solver' : ['randomized']
+                            param_grid = {'scaler__n_components':[250, 400, 500, 600, 750],
+                                          'scaler__kernel':['poly', 'cosine'],
                                           },
                             scoring = 'balanced_accuracy',
                             cv = 5,
